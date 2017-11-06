213f1937177fe8aa1b25425f8e21c5dd337b12d2
Andy-Curtis <Andy7Curtis@gmail.com>
2017-11-05 23:24:16 -0600

    Added functionality for splitting a clause into fragments

--- FearFactor.py
+++ FearFactor.py
@@ -1,1 +1,11 @@
-print("Hello")
+
+def ClauseFeeder(Clause):
+    print("Hello")
+    for x in range (0, len(Clause)):
+        print(Clause[:x])
+
+
+
+
+
+ClauseFeeder("Thequickbrownfoxjumpedoverthelazydog.")
