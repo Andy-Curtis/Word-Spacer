220f00aa3487695a95fa4ee64abae3839cc41191
Andy-Curtis <andy7curtis@gmail.com>
2017-11-08 19:46:52 -0600

    Renamed to parcer.py

--- FearFactor.py
+++ FearFactor.py
@@ -1,11 +1,16 @@
+from SymbolicRules import *
+from parcer import *
+
 
 def ClauseFeeder(Clause):
-    print("Hello")
     for x in range (0, len(Clause)):
         print(Clause[:x])
 
+buildDict()
+
+print(is_vowel_present("hghgfh"))
+print(is_vowel_present("dog"))
 
 
+#ClauseFeeder("Thequickbrownfoxjumpedoverthelazydog.")
 
-
-ClauseFeeder("Thequickbrownfoxjumpedoverthelazydog.")
